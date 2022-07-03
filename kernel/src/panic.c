4ad33ce480091afef085f4f984a7098fd0cd8203
Sergij Yevchuk <85248187+WinExperements@users.noreply.github.com>
2022-07-03 19:04:41 +0300

    Delete kernel/src directory

--- kernel/src/panic.c
+++
@@ -1,7 +0,0 @@
-#include "kernel.h"
-void panic(char *msg) {
-	printk("Kernel panic:");
-	printk(msg);
-	printk("\n");
-	for (;;) {}
-}
--- kernel/src/x86/idt.asm
+++
@@ -1,6 +0,0 @@
-[GLOBAL idt_flush]
-
-idt_flush:
-  mov eax , [esp+4]     ; Get the pointer to the idt struct
-  lidt [eax]            ; Loads the IDT
-  ret                   
